1839bed16030d7cb85cff48fcc2f60c8cd99fdf0
JEFFERSON FELICIO DE CARVALHO SEBASTIAO <jefferson.fcsebastia@senacsp.edu.br>
2019-12-10 20:25:16 -0300

    aula

---
+++ Terra plana/Assets/Scripts/Translacao.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translacao : MonoBehaviour
+{
+    public Transform Pai;
+    public float VelTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Pai.position, transform.up, VelTrans * Time.deltaTime);
+    }
+}
